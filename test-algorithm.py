2734303260afb73df8e2220901343022a23551e7
YzgllE <yzgll2362@icloud.com>
2025-06-22 18:06:23 +0300

    Update test-algorithm.py

--- test-algorithm.py
+++ test-algorithm.py
@@ -1,1 +1,20 @@
 
+import unittest
+from algorithm import rabin_karp
+
+class TestRabinKarp(unittest.TestCase):
+    def test_match(self):
+        text = "ABABDABACDABABCABAB"
+        pattern = "ABABC"
+        matches, _ = rabin_karp(text, pattern)
+        self.assertEqual(matches, [10])
+
+    def test_no_match(self):
+        text = "ABCDEFG"
+        pattern = "HIJ"
+        matches, _ = rabin_karp(text, pattern)
+        self.assertEqual(matches, [])
+
+if __name__ == '__main__':
+    unittest.main()
+
